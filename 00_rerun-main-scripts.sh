df8ea9b4527c43e1cdf3f4fb2ff6663ab014c799
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2020-11-23 17:11:19 -0500

    slimmer

--- 00_rerun-main-scripts.sh
+++ 00_rerun-main-scripts.sh
@@ -1,17 +1,13 @@
 # Run scripts
 set -o history -o histexpand
-# Rscript 01_initilize_dirs.R || exit 1
-Rscript 03_download-538.R || exit 1
 
+# data
+Rscript 03_download-538.R || exit 1
 Rscript 04a_standardize-538-2016-polls.R || exit 1
 Rscript 04b_standardize-538-2020-polls.R || exit 1
 Rscript 05_standardize_2016-2020.R || exit 1
 
-# Rscript 08_elec_results.R || exit 1
-# Rscript 21_clean-for-exploration.R || exit 1
-# Rscript 50_sim_2020_prep.R || exit 1
-# echo "finished ["!:0"] on script ["!:*"]"
-
+# Model
 Rscript 51_sim_2020_base.R || exit 1
 echo "finished ["!:0"] on script ["!:*"]"
 
@@ -22,11 +18,13 @@ Rscript 53_sim_2020_wdc.R || exit 1
 echo "finished ["!:0"] on script ["!:*"]"
 
 
-# Generate tables and counts (do figures by hand)
+# Tables and numebrs
 Rscript 71_tabulate-counts_tab01.R || exit 1
-echo "finished ["!:0"] on script ["!:*"]"
-
 Rscript 72_tabulate_rho-2016_tab02.R || exit 1
-echo "finished ["!:0"] on script ["!:*"]"
 
+# Figures
+Rscript 73_vis-hist_fig01.R || exit 1
+Rscript 74_vis-timewindow_fig02.R || exit 1
+Rscript 75_vis-by-N_fig03.R || exit 1
+Rscript 76_vis-sens_fig04.R || exit 1
 

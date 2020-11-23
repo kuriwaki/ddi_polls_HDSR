a1857bb5efe826b35bacab73a2bd863acae4b5c1
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2020-11-23 17:49:48 -0500

    Move to code subdir

--- 00_init-copy-from-private.R
+++ 00_init-copy-from-private.R
@@ -5,9 +5,11 @@ library(fs)
 pr_dir <- "~/Dropbox/ddi_polls"
 
 
-file_copy(dir_ls(pr_dir,
+dir_create("code")
+file_copy(path = dir_ls(pr_dir,
                  regexp = "(00|01|03|04a|04b|05|44|5[0-3]|7[0-6])_"),
-          new_path = ".", overwrite = TRUE)
+          new_path = "code",
+          overwrite = TRUE)
 
 
 
